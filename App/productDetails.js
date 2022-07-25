3c6e2611b4fbcee5914a8523b69bae8f0f9a5c7c
Justice Isreal <justiceyba@gmail.com>
2022-07-25 19:19:10 +0100

    design of client page

--- App/productDetails.js
+++ App/productDetails.js
@@ -38,4 +38,21 @@ addCart.forEach((item, index) => {
   });
 });
 
+// for toggle bar effect
+let menuBar = document.querySelector(".bx-menu");
+let leftNav = document.querySelectorAll(".left");
+let rightNav = document.querySelectorAll(".right");
 
+leftNav.forEach(function (nav) {
+  menuBar.addEventListener("click", () => {
+    nav.classList.toggle("active");
+    console.log(leftNav);
+    menuBar.classList.toggle("active");
+  });
+});
+rightNav.forEach(function (nav) {
+  menuBar.addEventListener("click", () => {
+    nav.classList.toggle("active");
+    console.log(leftNav);
+  });
+});
